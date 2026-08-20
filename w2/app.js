29cba9f2b808aa55f077ca5742fdd0dd96571efb
Armando-Hernandez1208 <1089262@my.scccd.edu>
2026-08-20 20:42:02 +0000

    step 2.3 complete

--- w2/app.js
+++ w2/app.js
@@ -10,5 +10,26 @@ document.addEventListener('DOMContentLoaded', () => {
    // Step 2.2: Variable Cleanup
   let totalCost = 0;     // replaces clickCount
   const itemPrice = 15;  // new constant
-  
+
+
+  // Step 2.3: Update Function Logic
+  const handleAddItem = function () {
+    // Add item price to total
+    totalCost += itemPrice;
+
+    // Build message
+    let message = `Current Total: $${totalCost}`;
+
+    // Budget logic
+    if (totalCost >= 60) {
+      message += " (Over Budget!)";
+      totalDisplayElement.style.color = "red";
+    } else {
+      totalDisplayElement.style.color = "green";
+    }
+
+    // Update display
+    totalDisplayElement.textContent = message;
+  };
+
 });
