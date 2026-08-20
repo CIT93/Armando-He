ecdb08defd2ecca25763d4b7f39ba7d656cfa259
Armando-Hernandez1208 <1089262@my.scccd.edu>
2026-08-20 20:38:20 +0000

    step 2.2 complete

--- w2/app.js
+++ w2/app.js
@@ -6,4 +6,9 @@ document.addEventListener('DOMContentLoaded', () => {
   // Step 2.1: Updated selectors
   const totalDisplayElement = document.getElementById("total-display");
   const addItemButtonElement = document.getElementById("add-item-btn");
+
+   // Step 2.2: Variable Cleanup
+  let totalCost = 0;     // replaces clickCount
+  const itemPrice = 15;  // new constant
+  
 });
