118f7ef7a44e54fc052b54df47cc96e7d9a94560
Armando-Hernandez1208 <1089262@my.scccd.edu>
2026-09-15 04:59:38 +0000

    step 4 complete

--- w6a/app.js
+++ w6a/app.js
@@ -1,16 +1,12 @@
 import * as orderForm from "./order-handler.js";
 import * as priceCalculator from "./price-calculator.js";
 import * as resultsDisplay from "./results-display.js";
-
-
+import * as orderStorage from './order-storage.js';
 
 const orders = [];
 
 // Get a reference to the order form
 const form = document.querySelector("#order-form");
-
-// Get a reference to the order summary
-
 
 // Handles the order form submission
 const handleOrderSubmit = (event) => {
@@ -27,13 +23,24 @@ const handleOrderSubmit = (event) => {
 
   orders.push(newOrder);
 
+  // PASO 5 (se hace después del paso 4)
+  orderStorage.saveOrders(orders);
+
   resultsDisplay.displayOrder(newOrder);
 }
 
-
- 
 // Initializes the application
 const init = () => {
+
+  // ⭐⭐⭐ PASO 4 — Cargar órdenes guardadas al iniciar ⭐⭐⭐
+  const loadedOrders = orderStorage.loadOrders();
+
+  if (loadedOrders.length > 0) {
+    orders.push(...loadedOrders);
+    console.log("Orders loaded");
+  }
+
+  // Event listener
   form.addEventListener("submit", handleOrderSubmit);
 
   console.log("App Initialized");
@@ -41,5 +48,3 @@ const init = () => {
 
 // Start the application when the DOM is ready
 document.addEventListener("DOMContentLoaded", init);
-
-
